ef0057c3b4c91705cefbef2e5bd14344fc85f850
itskristyy <adinda.kristyy1903@gmail.com>
2026-07-21 10:48:58 +0700

    chore: create guestbook Logic

---
+++ function/function_guestbook/add_message.php
@@ -0,0 +1,16 @@
+<?php
+include '../connect.php';
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+date_default_timezone_set('Asia/Jakarta');
+$createdAt = date('d-m-Y');
+
+$sql = "INSERT INTO tb_guestbook (nama, email, message, createdAt) VALUES ('$nama', '$email', '$message', '$createdAt')";
+$result = mysqli_query($koneksi, $sql);
+if ($result) {
+    header("Location: ../../index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+}
