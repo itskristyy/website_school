b23a747d4e696f88b48bbf746064a31ff2d3b296
NaApipp <nabilapipp@gmail.com>
2026-07-21 22:32:52 +0700

    fix: fixing path direct after succes add guru

--- function/function_guru/add_guru.php
+++ function/function_guru/add_guru.php
@@ -5,11 +5,11 @@ $nama_guru = $_POST['nama_guru'];
 $guru_jurusan = $_POST['guru_jurusan'];
 $nip = $_POST['nip'];
 
-$sql = "INSERT INTO tb_alumni (nama_guru, guru_jurusan, nip) 
+$sql = "INSERT INTO tb_guru (nama_guru, guru_jurusan, nip) 
                         VALUES ('$nama_guru', '$guru_jurusan', '$nip')";
 $result = mysqli_query($koneksi, $sql);
 if ($result) {
-    header("Location: ../../page/dashboard/data_alumni.php");
+    header("Location: ../../page/dashboard/data_guru.php");
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
 }
