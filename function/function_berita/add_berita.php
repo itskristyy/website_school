a60be3fdd36465c5983afd42aac4277450a63896
itskristyy <adinda.kristyy1903@gmail.com>
2026-07-21 10:49:29 +0700

    chore: create berita Logic

---
+++ function/function_berita/add_berita.php
@@ -0,0 +1,18 @@
+<?php
+include '../connect.php';
+
+$judul_berita = $_POST['judul_berita'];
+$url_image = $_POST['url_image'];
+$deskripsi_berita = $_POST['deskripsi_berita'];
+$kategori_berita = $_POST['kategori_berita'];
+$created_at = date('Y-m-d');
+$updated_at = date('Y-m-d');
+
+$sql = "INSERT INTO tb_berita (judul_berita, url_image, deskripsi_berita, kategori_berita, created_at, updated_at) 
+                        VALUES ('$judul_berita', '$url_image', '$deskripsi_berita', '$kategori_berita', '$created_at', '$updated_at')";
+$result = mysqli_query($koneksi, $sql);
+if ($result) {
+    header("Location: ../../index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+}
---
+++ function/function_berita/edit_berita.php
@@ -0,0 +1,21 @@
+<?php
+include '../connect.php';
+
+// import data
+$id_berita = $_GET['id_berita'];
+$judul_berita = $_POST['judul_berita'];
+$url_image = $_POST['url_image'];
+$deskripsi_berita = $_POST['deskripsi_berita'];
+$kategori_berita = $_POST['kategori_berita'];
+$created_at = $_POST['created_at'];
+$updated_at = $_POST['updated_at'];
+
+$sql = mysqli_query($koneksi, "UPDATE tb_berita
+                              SET judul_berita='$judul_berita', url_image='$url_image', deskripsi_berita='$deskripsi_berita', kategori_berita='$kategori_berita', created_at='$created_at', updated_at='$updated_at' 
+                              WHERE id_berita='$id_berita'");
+
+if ($sql) {
+    header("Location: ../../index.php?msg=berhasil");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+}
